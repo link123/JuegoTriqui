f0de3bdb001ca0a07825b0c5f86e74b775781ae5
DiegoRuiz <diegoruizdx@gmail.com>
2022-01-22 02:00:58 -0500

    triqui con funciones refactorizadas

--- script.js
+++ script.js
@@ -1,111 +1,138 @@
 // Manejo de las casillas
-let turno = 0;
-const tablero = [];
-const nodes = document.getElementsByTagName('button');
+let turn = 0;
+const board = [];
+const boxList = document.getElementsByTagName('button');
+const player1Won = document.getElementById("player1");
+const player2Won = document.getElementById("player2");
 
-function myClick(e, index) {
-    const btn = e.target;
-
-    btn.disabled = true;
-
-    turno++;
-
-    pintar(turno, btn, index);
-
-
-}
-
-function pintar(turno, btn, index) {
-
-    if (turno % 2 === 0) {
-        console.log();
-
-        btn.innerHTML = 'O';
-        btn.style.fontSize = `${50}px`;
-        llenarTablero(index, btn, 'O');
-    } else {
-        btn.innerHTML = 'X';
-        btn.style.fontSize = `${50}px`;
-        llenarTablero(index, btn, 'X');
-    }
-
-
-}
-
-function llenarTablero(index, btn, XO) {
-    tablero[index] = XO;
-    console.log(index + ' ' + XO);
-    validarGanador();
-}
-
-function validarGanador() {
-
-    if (tablero[0] == tablero[1] && tablero[1] == tablero[2] && tablero[0]) {
-        nodes[0].style.backgroundColor = 'red';
-        nodes[1].style.backgroundColor = 'red';
-        nodes[2].style.backgroundColor = 'red';
-        setTimeout(function mostrar() { alert('gano'); }, 1000);
-    }
-    if (tablero[3] == tablero[4] && tablero[4] == tablero[5] && tablero[3]) {
-        nodes[3].style.backgroundColor = 'red';
-        nodes[4].style.backgroundColor = 'red';
-        nodes[5].style.backgroundColor = 'red';
-
-        setTimeout(function mostrar() { alert('gano'); }, 1000);
-    }
-    if (tablero[6] == tablero[7] && tablero[7] == tablero[8] && tablero[6]) {
-        nodes[6].style.backgroundColor = 'red';
-        nodes[7].style.backgroundColor = 'red';
-        nodes[8].style.backgroundColor = 'red';
-
-        setTimeout(function mostrar() { alert('gano'); }, 1000);
-    }
-
-
-    if (tablero[0] == tablero[3] && tablero[3] == tablero[6] && tablero[0]) {
-
-        nodes[0].style.backgroundColor = 'red';
-        nodes[3].style.backgroundColor = 'red';
-        nodes[6].style.backgroundColor = 'red';
-
-        setTimeout(function mostrar() { alert('gano'); }, 1000);
-    }
-    if (tablero[1] == tablero[4] && tablero[4] == tablero[7] && tablero[1]) {
-        nodes[1].style.backgroundColor = 'red';
-        nodes[4].style.backgroundColor = 'red';
-        nodes[7].style.backgroundColor = 'red';
-
-        setTimeout(function mostrar() { alert('gano'); }, 1000);
-    }
-    if (tablero[2] == tablero[5] && tablero[5] == tablero[8] && tablero[2]) {
-        nodes[2].style.backgroundColor = 'red';
-        nodes[5].style.backgroundColor = 'red';
-        nodes[8].style.backgroundColor = 'red';
-
-        setTimeout(function mostrar() { alert('gano'); }, 1000);
-    }
-
-
-    if (tablero[0] == tablero[4] && tablero[4] == tablero[8] && tablero[0]) {
-        nodes[0].style.backgroundColor = 'red';
-        nodes[4].style.backgroundColor = 'red';
-        nodes[8].style.backgroundColor = 'red';
-
-        setTimeout(function mostrar() { alert('gano'); }, 1000);
-    }
-    if (tablero[6] == tablero[4] && tablero[4] == tablero[2] && tablero[6]) {
-
-        nodes[6].style.backgroundColor = 'red';
-        nodes[4].style.backgroundColor = 'red';
-        nodes[2].style.backgroundColor = 'red';
-
-        setTimeout(function mostrar() { alert('gano'); }, 1000);
-    }
-
-
-
-
-}
 
 document.querySelectorAll("button").forEach((obj, i) =>
-    obj.addEventListener("click", (e) => myClick(e, i)));
+    obj.addEventListener("click", (e) => checkBox(e, i)));
+
+
+function checkBox(e, index) {
+    const box = e.target;
+    box.disabled = true;
+    turn++;
+    drawMark(turn, box, index);
+}
+
+
+
+function drawMark(turn, box, index) {
+
+    if (turn % 2 === 0) {
+        box.innerHTML = 'O';
+        box.style.fontSize = `${55}px`;
+        markBoard(index, box, 'O');
+    } else {
+        box.innerHTML = 'X';
+        box.style.fontSize = `${55}px`;
+        markBoard(index, box, 'X');
+    }
+
+}
+
+function markBoard(index, box, XO) {
+    board[index] = XO;
+    validateWinnerExist();
+}
+
+function validateWinnerExist() {
+
+    validateRow1();
+    validateRow2();
+    validateRow3();
+
+    validateColumn1();
+    validateColumn2();
+    validateColumn3();
+
+    validateDiagonal1()
+    validateDiagonal2();
+
+}
+
+function validateRow1() {
+    if (board[0] == board[1] && board[1] == board[2] && board[0]) {
+        drawLine(0, 1, 2);
+        showMessageWinner(board[0]);
+    }
+}
+
+function validateRow2() {
+    if (board[3] == board[4] && board[4] == board[5] && board[3]) {
+        drawLine(3, 4, 5);
+        showMessageWinner(board[3]);
+    }
+}
+
+function validateRow3() {
+    if (board[6] == board[7] && board[7] == board[8] && board[6]) {
+        drawLine(6, 7, 8);
+        showMessageWinner(board[6]);
+    }
+}
+
+function validateColumn1() {
+
+    if (board[0] == board[3] && board[3] == board[6] && board[0]) {
+        drawLine(0, 3, 6);
+        showMessageWinner(board[0]);
+    }
+}
+
+function validateColumn2() {
+
+    if (board[1] == board[4] && board[4] == board[7] && board[1]) {
+        drawLine(1, 4, 7);
+        showMessageWinner(board[1]);
+    }
+}
+
+function validateColumn3() {
+
+    if (board[2] == board[5] && board[5] == board[8] && board[2]) {
+        drawLine(2, 5, 8);
+        showMessageWinner(board[2]);
+    }
+}
+
+function validateDiagonal1() {
+
+    if (board[0] == board[4] && board[4] == board[8] && board[0]) {
+        drawLine(0, 4, 8);
+        showMessageWinner(board[0]);
+    }
+}
+
+function validateDiagonal2() {
+
+    if (board[6] == board[4] && board[4] == board[2] && board[6]) {
+        drawLine(6, 4, 2);
+        showMessageWinner(board[6]);
+
+    }
+}
+
+
+function drawLine(arg1, arg2, arg3) {
+    boxList[arg1].style.color = 'red';
+    boxList[arg2].style.color = 'red';
+    boxList[arg3].style.color = 'red';
+    disableAllBoard();
+}
+
+function disableAllBoard() {
+    for (let i = 0; i < boxList.length; i++) {
+        boxList[i].disabled = true;
+    }
+
+}
+
+function showMessageWinner(mark) {
+    if (mark === 'X')
+        player1Won.style.color = 'red';
+    else
+        player2Won.style.color = 'red';
+}
